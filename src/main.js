b1604206b436c96271ed34f6f534ad52e51ea2ad
Brendson <brendson.net@gmail.com>
2022-06-19 11:30:05 -0300

    style: styling the alerts

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 
-createApp(App).mount('#app');
+const app = createApp(App)
+
+import VueSweetalert2 from 'vue-sweetalert2';
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+app.use(VueSweetalert2);
+
+app.mount('#app');
